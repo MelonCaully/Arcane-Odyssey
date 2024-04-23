da9a31a45ce83e316d50bfd66bd63ab58d5d1dd5
Dawg <118694140+Xorace@users.noreply.github.com>
2024-04-22 23:35:05 -0400

    ez menu port
    gg

---
+++ Main Menu Port/Main Menu/Main Menu/UI Script/Scripts/StateMachine/StateMachine.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class StateMachine : MonoBehaviour
+{
+    private BaseState currentState;
+
+    [SerializeField]
+    private UIRoot ui;
+    public UIRoot UI => ui;
+
+    private void Start()
+    {
+        ChangeState(new MenuState());
+    }
+
+    private void Update()
+    {
+        if (currentState != null)
+        {
+            currentState.UpdateState();
+        }
+    }
+
+    public void ChangeState(BaseState newState)
+    {
+
+        if (currentState != null)
+        {
+            currentState.DestroyState();
+        }
+
+        currentState = newState;
+
+        if (currentState != null)
+        {
+            currentState.owner = this;
+            currentState.PrepareState();
+        }
+    }
+}
+
